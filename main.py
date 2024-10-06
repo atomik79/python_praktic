41749346934edbe2013f8600afafc89c89183b5e
atomik79 <162672087+atomik79@users.noreply.github.com>
2024-10-06 22:04:36 +1200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+
+
+  # 1-st program
+print((9**0.5) *5)
+
+  # 2-nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+  #3-rd program
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print((2 * 2 + 2) == (2 * (2 + 2)))
+
+  #4-th program
+s = "123.456"
+s_2 = float(s) * 10
+s_3 = int(s_2) % 10
+print(s_3)
