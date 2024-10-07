4ebca48b20eeaeb0f30227611937e35ea686fc66
atomik79 <162672087+atomik79@users.noreply.github.com>
2024-10-07 23:37:14 +1200

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,5 @@
+home_task = 12
+count_hours = 1.5
+name_course = "Python"
+time_tasks = count_hours / home_task
+print("Курс: " + name_course + ",всего задач:" + str(home_task) + "," + "затрачено часов:",1.5,"среднее время выполнения",0.125,"часа." )
