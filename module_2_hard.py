320726d060ef561973ab9354cc65287aef98a102
atomik79 <162672087+atomik79@users.noreply.github.com>
2024-10-30 21:43:41 +1200

    Add files via upload

---
+++ module_2_hard.py
@@ -0,0 +1,39 @@
+import random
+
+
+def list_digit():
+    # list_dig = []
+    # for i in range(20):
+    # list_dig.append(randint(3, 20))
+    # return list_dig
+    num = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    digit = random.choice(num)
+    return digit
+
+n = list_digit()
+print(n)
+
+def get_dict(n):
+    dict_2 = {}
+    dict_2.update({3: 12, 4: 13, 5: 1423, 6: 1524, 7: 162534, 8: 172635, 9: 18273645})
+    dict_2.update({10: 1923283746, 11: 11029384756, 12: 12131511124210394857, 13: 112211310495867})
+    dict_2.update({14: 1611325212343114105968, 15: 1214114232133124115106978})
+    dict_2.update({16: 1317115262143531341251161079, 17: 11621531441351261171089})
+    dict_2.update({18: 12151811724272163631545414513612711810, 19: 118217316415514613712811910})
+    dict_2.update({20: 1192183731746416515614713812911})
+    dict_1 = dict_2.get(n)
+    return dict_1
+
+dig_1 = list(range(1, n))
+dig_2 = list(range(1, n))
+digits = []
+result = ''
+
+for i in dig_1:
+    for j in dig_2:
+        if n % (i + j) == 0:
+            digits.append([i, j])
+            result += str(i) + str(j)
+print('number', *digits)
+print('code', result)
+print('Код совпал(наверное)!')
