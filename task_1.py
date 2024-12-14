367e4c72bd7876437bc8e774d41fe4d24c0f0019
atomik79 <162672087+atomik79@users.noreply.github.com>
2024-12-15 06:27:48 +1000

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,17 @@
+from function import str_2
+
+#first programm
+
+print((9 ** 0.5) * 5)
+
+      #second programm
+
+print(9.99 > 9.98 and 1000 < 1000.1 )
+
+     #fthird programm
+
+print(2 * 2 + 2 == 2 * (2 + 2))
+
+    #fourth programm
+
+print(int(float('123.456') * 10) % 10)
